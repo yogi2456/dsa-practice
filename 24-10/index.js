84e6aaca39cd3462a286513693f29105f4feffa7
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-25 18:26:15 +0530

    updates

--- 24-10/index.js
+++ 24-10/index.js
@@ -1,9 +1,9 @@
 //remove duplicate in sorted array in leetcode question 26?
 
-nums = [0,1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6]
+nums = [1, 1, 2, 2, 3, 3, 3, 7, 7, 4, 4, 5, 6, 6]
 
 function duplicate(nums) {
-    nums.sort()
+    //nums.sort()
     var j = 1;
     for(var i = 0; i < nums.length - 1; i++) {
         //console.log(nums[i])
@@ -17,35 +17,3 @@ function duplicate(nums) {
 
 console.log(duplicate(nums))
 
-let value = "10";
-
-function palindrome() {
-    //var reverseStr = x.toString().split("").reverse().join("")
-    //return (x.toString() == reverseStr)
-    let reverseVal = value.toString().split("").reverse().join("")
-    let stringVal = value.toString()
-    if(reverseVal == stringVal) {
-        return true;
-    }
-    return false;
-}
-
-console.log(palindrome(value))
-
-
-var x = "-121";
-function checkP(x) {
-    // if(str.length < 2){
-    //     return "Str not valid"
-    // }
-
-    for (var i = 0; i < x.length / 2; i++) {
-        if (x[i] !== x[x.length - 1 - i]) {
-            return false
-        }
-    }
-    return true
-
-}
-
-console.log(checkP(x))
