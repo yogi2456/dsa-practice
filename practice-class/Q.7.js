7135ef8cd184bc8cb4a736600ec7410e57753d4f
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-25 18:22:10 +0530

    updates

--- practice-class/Q.7.js
+++ practice-class/Q.7.js
@@ -9,20 +9,31 @@
 // var x = 231;
 var x = 67856743;
 
+// function reverseInteger(x) {
+//     var reversed = 0;
+
+//     while (x !== 0) {
+//         reversed = reversed * 10 + x % 10;
+//         x = Math.trunc(x / 10);
+//     }
+
+//     if(reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1) {
+//         return 0;
+//     }
+//     return reversed;
+// }
+
+// console.log(reverseInteger(x));
+
 function reverseInteger(x) {
-    var reversed = 0;
+    //var reversed = 0;
 
-    while (x !== 0) {
-        reversed = reversed * 10 + x % 10;
-        x = Math.trunc(x / 10);
+    for(var i = 0; i < x.length; i++) {
+        console.log(i)
+        x.reverse();
     }
-
-    if(reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1) {
-        return 0;
-    }
-    return reversed;
 }
 
-console.log(reverseInteger(x));
+console.log(reverseInteger(x))
 
 
--- practice-class/Q.9.js
+++ practice-class/Q.9.js
@@ -1,32 +1,34 @@
-let value = "101";
+//let value = "101";
+let x = "134";
 
 function palindrome() {
-    //var reverseStr = x.toString().split("").reverse().join("")
-    //return (x.toString() == reverseStr)
-    let reverseVal = value.toString().split("").reverse().join("")
-    let stringVal = value.toString()
-    if(reverseVal == stringVal) {
-        return true;
-    }
-    return false;
-}
-
-console.log(palindrome(value))
-
-
-var x = "121";
-function checkP(x) {
-    // if(str.length < 2){
-    //     return "Str not valid"
+    var reverseStr = x.toString().split("").reverse().join("")
+    return (x.toString() == reverseStr)
+    // let reverseVal = value.toString().split("").reverse().join("")
+    // let stringVal = value.toString()
+    // if(reverseVal == stringVal) {
+    //     return true;
     // }
-
-    for (var i = 0; i < x.length / 2; i++) {
-        if (x[i] !== x[x.length - 1 - i]) {
-            return false
-        }
-    }
-    return true
-
+    // return false;
 }
 
-console.log(checkP(x))
+//console.log(palindrome(value))
+console.log(palindrome(x))
+
+
+// var x = "121";
+// function checkP(x) {
+//     // if(str.length < 2){
+//     //     return "Str not valid"
+//     // }
+
+//     for (var i = 0; i < x.length / 2; i++) {
+//         if (x[i] !== x[x.length - 1 - i]) {
+//             return false
+//         }
+//     }
+//     return true
+
+// }
+
+// console.log(checkP(x))
