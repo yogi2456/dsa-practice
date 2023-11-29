ceb3a1ce13a783bf997fcc8c6fee81a0155ad16a
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-11-29 20:26:28 +0530

    updates

--- test/test3.js
+++ test/test3.js
@@ -3,17 +3,13 @@
 //input = [2, 8, 5, 1, 9];
 //output = 9;
 
-// var array = [2, 8, 5, 1, 9];
+var array = [2, 8, 5, 1, 9];
 
-// function maximumValue(array) {
-//     for(var i = 0; i < array.length; i++) {
-//         //console.log(array[i])
-//         maxValue = Math.max(array)
-//     }
-//     return maxValue;
-// }
+function maximumValue(array) {
+    maxValue = Math
+}
 
-// console.log(maximumValue(array))
+console.log(maximumValue(array))
 
-var a = Math.max(2, 8, 5, 1, 9);
-console.log(a)
+// var a = Math.max(2, 8, 5, 1, 9);
+// console.log(a)
